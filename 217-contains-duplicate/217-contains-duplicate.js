0c859442c60727f9e44845c04224c26bf14dfb48
Junho Won <45681336+junjunwon@users.noreply.github.com>
2022-05-11 01:50:32 +0900

    Time: 163 ms (28.49%), Space: 50.8 MB (66.81%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    const map = new Map();
+    
+    for(let i = 0; i<nums.length; i++) {
+        if(map.has(nums[i])) {
+            return true
+        }
+        map.set(nums[i], nums[i])
+    }
+    
+    return false
+};
