de143726aee8a9b8893de6a37f8c55cdb4856ac1
Junho Won <45681336+junjunwon@users.noreply.github.com>
2023-07-05 21:00:31 +0900

    Time: 21 ms (17.28%), Space: 57.7 MB (5.88%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        
+        List<List<Integer>> results = new ArrayList();
+        
+        Arrays.sort(arr);
+        int miniGap = 99999;
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            int gap = Math.abs(arr[i] - arr[i+1]);
+            if (gap < miniGap) {
+                miniGap = gap;
+            }
+        }
+        
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = i+1; j < arr.length; j++) {
+//                 int gap = Math.abs(arr[i] - arr[j]);
+//                 if (gap < miniGap) {
+//                     miniGap = gap;
+//                 }
+//             }
+//         }
+        
+        
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+            List<Integer> result = new ArrayList();
+            int gap = Math.abs(arr[i] - arr[i+1]);
+            if (gap == miniGap) {
+                result.add(arr[i]);
+                result.add(arr[i+1]);
+                results.add(result);
+            }
+        }
+        
+        return results;
+    }
+}
