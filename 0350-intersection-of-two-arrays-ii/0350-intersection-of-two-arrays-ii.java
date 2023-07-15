362cb83ea0ffa637359826922bb7aba092efb7c6
Junho Won <45681336+junjunwon@users.noreply.github.com>
2023-07-16 08:30:00 +0900

    Time: 7 ms (8.63%), Space: 43.5 MB (47.75%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -4,6 +4,65 @@ class Solution {
     4 4 8 9 9
     **/
     public int[] intersect(int[] nums1, int[] nums2) {
+        // (1 2 2 1), (2,2)
+
+        // num1, num2 array
+
+        // 1
+        // hold the key, value into the map (key: then number,value; count) 
+        // num1 map, num2 map
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            int target = nums1[i];
+            map1.put(target, map1.getOrDefault(target, 0) + 1);
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            int target = nums2[i];
+            map2.put(target, map2.getOrDefault(target, 0) + 1);
+        }
+
+        // 2
+        // 4 9 5
+        // 9 4 9 8 4
+
+        // map1: 4->1, 9->1, 5->1
+        // map2: 4->2, 9->2, 8->1
+
+        // finding duplicate keys between two maps -> candidate keys 
+        List<Integer> keys = new ArrayList();
+        for (int map1Key: map1.keySet()) {
+            if (map2.containsKey(map1Key)) {
+                keys.add(map1Key);
+            }
+        }
+
+        // compare num1[key] num2[key] append Min value to result
+
+        // 3
+        // append keys (which counts the value) to result list
+        // List<Integer> resultKey = new ArrayList();
+
+        List<Integer> result = new ArrayList();
+        
+        for (int i = 0; i < keys.size(); i++) {
+            int target = keys.get(i);
+            int t1 = map1.get(target);
+            int t2 = map2.get(target);
+
+            int targetValue = Math.min(t1, t2);
+
+            for (int j = 0; j < targetValue; j++) {
+                result.add(target);
+            }
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    public int[] intersect0(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         List<Integer> temp = new ArrayList();
