d737a1a7191a6fd9b6b7eaf75e8649d6594fa851
Junho Won <45681336+junjunwon@users.noreply.github.com>
2023-07-15 21:42:44 +0900

    Time: 2 ms (95.84%), Space: 42.3 MB (74.22%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,5 +1,52 @@
 class Solution {
+    /**
+    4 5 9
+    4 4 8 9 9
+    **/
     public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int i = 0, j = 0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]) i++;
+            else if(nums1[i] > nums2[j]) j++;
+            else{
+                arr.add(nums1[i]);
+                i++;j++;
+            }
+        }
+        int[] result = new int[arr.size()];
+        for(int k=0;k < arr.size();k++){
+            result[k] = arr.get(k);
+        }
+        return result;
+    }
+    public int[] intersect0(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> temp = new ArrayList();
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] > nums2[j]) {
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                // nums1[i] == nums2[j]
+                temp.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] result = new int[temp.size()];
+        for (int k = 0; k < result.length; i++) {
+            result[k] = temp.get(k);
+        }
+        return result;
+    }
+    public int[] intersect1(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap();
         List<Integer> temp = new ArrayList();
         
