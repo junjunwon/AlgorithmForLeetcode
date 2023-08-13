7a9e4e1e368aa609364dfb86a07faeba934aa29c
Junho Won <45681336+junjunwon@users.noreply.github.com>
2023-08-13 18:45:33 +0900

    Time: 6 ms (22.94%), Space: 44.2 MB (48.95%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,26 @@
+/**
+numsSize = nums.length;
+first : find numsSize; true? output
+second : find missing number.
+ */
+class Solution {
+    public int missingNumber(int[] nums) {
+        int size = nums.length;
+        Arrays.sort(nums);
+        if (nums[size-1] != size) {
+            return size;
+        }
+
+/**
+index : 0 1 2
+value : 0 1 3
+ */
+    int find = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i+1] > nums[i] + 1) {
+                find = nums[i+1] - 1;
+            }
+        }
+        return find;
+    }
+}
