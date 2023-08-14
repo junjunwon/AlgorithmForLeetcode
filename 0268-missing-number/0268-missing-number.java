26dd9087cbfe46c15b18306d8dac114821efdc7f
Junho Won <45681336+junjunwon@users.noreply.github.com>
2023-08-14 21:43:28 +0900

    Time: 6 ms (22.74%), Space: 43.8 MB (94.52%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,9 +2,30 @@
 numsSize = nums.length;
 first : find numsSize; true? output
 second : find missing number.
+3 
++= 0 - 3 = -3 
++= 1 - 0 = 1
++= 2 - 1 = 1
+
+
+2 0 1
+3
++= 0
+
+
  */
 class Solution {
     public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int lookedFor = nums[i]^i; // 3, 0
+            if (lookedFor != 0) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+    public int missingNumber2(int[] nums) {
         int totalSum = 0;
         int missingSum = 0;
 
@@ -24,11 +45,6 @@ class Solution {
         if (nums[size-1] != size) {
             return size;
         }
-
-/**
-index : 0 1 2
-value : 0 1 3
- */
         for (int i = 0; i < nums.length-1; i++) {
             if (nums[i+1] > nums[i] + 1) {
                 return nums[i+1] - 1;
